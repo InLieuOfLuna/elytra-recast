1d3aef076777bcc07bb842496096de215e49fbb7
Luna <luna@lunaluna.me>
2021-09-16 23:45:21 +0100

    Initial Commit

---
+++ src/main/java/me/lunaluna/fabric/elytrarecast/ElytraRecast.java
@@ -0,0 +1,10 @@
+package me.lunaluna.fabric.elytrarecast;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ElytraRecast implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/lunaluna/fabric/elytrarecast/client/ElytraRecastClient.java
@@ -0,0 +1,13 @@
+package me.lunaluna.fabric.elytrarecast.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ElytraRecastClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
