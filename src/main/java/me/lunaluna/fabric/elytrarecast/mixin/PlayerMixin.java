74907cd00b2b8e66808be9db4a3f2d9ab616b16e
Luna <luna@lunaluna.me>
2021-09-17 12:43:21 +0100

    Elytra now casts whenever possible

--- src/main/java/me/lunaluna/fabric/elytrarecast/mixin/PlayerMixin.java
+++ src/main/java/me/lunaluna/fabric/elytrarecast/mixin/PlayerMixin.java
@@ -1,14 +1,47 @@
 package me.lunaluna.fabric.elytrarecast.mixin;
 
+import com.mojang.authlib.GameProfile;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.input.Input;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ElytraItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayerEntity.class)
 @Environment(EnvType.CLIENT)
-public class PlayerMixin {
+public abstract class PlayerMixin extends AbstractClientPlayerEntity {
 
-    //TODO: Implement recasting elytra when landing from elytra jumping
+    @Shadow public Input input;
+    @Shadow @Final public ClientPlayNetworkHandler networkHandler;
 
+    public PlayerMixin(ClientWorld clientWorld, GameProfile gameProfile) {
+        super(clientWorld, gameProfile);
+    }
+
+    @Inject(method = "tickMovement", at = @At("TAIL"))
+    public void recastIfLanded(CallbackInfo ci) {
+        tryToCast();
+    }
+
+    public void tryToCast() {
+        if (this.input.jumping && !this.getAbilities().flying && !this.hasVehicle() && !this.isClimbing()) {
+            ItemStack itemStack = this.getEquippedStack(EquipmentSlot.CHEST);
+            if (itemStack.isOf(Items.ELYTRA) && ElytraItem.isUsable(itemStack) && this.checkFallFlying()) {
+                this.networkHandler.sendPacket(new ClientCommandC2SPacket(this, ClientCommandC2SPacket.Mode.START_FALL_FLYING));
+            }
+        }
+    }
 }
