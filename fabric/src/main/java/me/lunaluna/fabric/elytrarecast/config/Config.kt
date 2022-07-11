463cb450f75f2c75f2329db8b4b09f63d7216a50
luna <lunagibson@duck.com>
2022-07-11 19:49:08 +0100

    Transitioned Config and screen to AutoConfig

--- fabric/src/main/java/me/lunaluna/fabric/elytrarecast/config/Config.kt
+++ fabric/src/main/java/me/lunaluna/fabric/elytrarecast/config/Config.kt
@@ -1,8 +1,12 @@
 package me.lunaluna.fabric.elytrarecast.config
 
+import me.shedaniel.autoconfig.ConfigData
+import me.shedaniel.autoconfig.annotation.Config
+
+@Config(name = "elytra-recast")
 data class Config(
     var enabled: Boolean = false,
     var cooldown: Int = 0,
     var jumpEnabled: Boolean = false,
     var jumpCooldown: Int = 0
-)
+) : ConfigData
